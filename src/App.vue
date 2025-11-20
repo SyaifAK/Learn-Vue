1e522433327a08264932d9a9da92a716cd1b709e
SyaifAk <alkhalimsyaif@gmail.com>
2025-11-20 11:37:11 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+const name = 'Syaif';
+const year = 2025;
+</script>
+
+<template>
+  <main>
+    <h1>Halo, {{ name }}</h1>
+    <p>Learn Vue in {{ year }}</p>
+  </main>
+
+  <footer>
+    <p>© {{ name }} {{ year }}</p>
+  </footer>
+</template>
+
+<style scoped>
+main {
+  font-size: 50px;
+  text-align: center;
+}
+
+footer {
+  text-align: center;
+  height: 50px;
+  background-color: green;
+  color: white;
+}
+</style>
